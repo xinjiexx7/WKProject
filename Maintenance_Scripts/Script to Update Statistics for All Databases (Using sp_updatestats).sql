9a836434013be4423c8570aa8a161bf1e4fc5c01
xinjiexx7 <xinjie.xia11@gmail.com>
2025-06-17 14:54:49 -0400

    Update Script to Update Statistics for All Databases (Using sp_updatestats).sql

--- Maintenance_Scripts/Script to Update Statistics for All Databases (Using sp_updatestats).sql
+++ Maintenance_Scripts/Script to Update Statistics for All Databases (Using sp_updatestats).sql
@@ -1,20 +1,21 @@
-DECLARE @SQL VARCHAR(1000) 
-DECLARE @DB sysname 
+DECLARE @SQL NVARCHAR(1000);
+DECLARE @DB sysname;
 
 DECLARE curDB CURSOR FORWARD_ONLY STATIC FOR 
    SELECT [name] 
-   FROM master..sysdatabases
-   WHERE [name] NOT IN ('model', 'tempdb')
-   ORDER BY [name]
-    
-OPEN curDB 
-FETCH NEXT FROM curDB INTO @DB 
-WHILE @@FETCH_STATUS = 0 
-   BEGIN 
-       SELECT @SQL = 'USE [' + @DB +']' + CHAR(13) + 'EXEC sp_updatestats' + CHAR(13) 
-       PRINT @SQL 
-       FETCH NEXT FROM curDB INTO @DB 
-   END 
-   
-CLOSE curDB 
-DEALLOCATE curDB
+   FROM sys.databases
+   WHERE [name] NOT IN ('master', 'model', 'msdb', 'tempdb')
+   ORDER BY [name];
+
+OPEN curDB;
+FETCH NEXT FROM curDB INTO @DB;
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+    SET @SQL = N'USE [' + QUOTENAME(@DB) + ']; EXEC sp_updatestats;';
+    EXEC sp_executesql @SQL;
+    FETCH NEXT FROM curDB INTO @DB;
+END;
+
+CLOSE curDB;
+DEALLOCATE curDB;
